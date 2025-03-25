f2d99fe9e748f12d5a64459e4bee829b405bdd54
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-03-25 11:20:13 +0900

    최종 본

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import Routing from "./Routing";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <Routing />
-    </div>
+    <Routing />
   );
 }
 
--- src/Routing.tsx
+++ src/Routing.tsx
@@ -6,10 +6,10 @@ import Login from "./pages/Login";
 function Routing() {
   return (
     <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/login" element={<Login />} />
-  </Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   )
 }
 
