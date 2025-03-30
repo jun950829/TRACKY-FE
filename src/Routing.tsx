b9755a0ea3b1224512c307a8f44382a80b502e3a
admin <admin@adminui-MacBookAir.local>
2025-03-30 16:09:34 +0900

    로그인 페이지

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -2,14 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
+import Main from "./pages/main";
+
 
 function Routing() {
   return (
     <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/login" element={<Login />} />
-  </Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/main" element={<Main />} />
+    </Routes>
   )
 }
 
