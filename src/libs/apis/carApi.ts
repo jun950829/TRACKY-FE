f26ee7f3124f8c7eca441a0bfad914af737f5dcf
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-04-02 21:42:19 +0900

    feat: merge

--- src/libs/apis/carApi.ts
+++ src/libs/apis/carApi.ts
@@ -42,8 +42,8 @@ export const carApiService = {
 
   // 에뮬레이터용
   sendCycleInfo: async (data: CycleInfoRequest) => {
-    const response = await api.post(`http://tracky-hub-2020422079.ap-northeast-2.elb.amazonaws.com:8082/api/car/cycle`, data);
-    console.log("📦 전송할 주소:", `http://tracky-hub-2020422079.ap-northeast-2.elb.amazonaws.com:8082/api/car/cycle`);
+    const response = await api.post(`/api/car/cycle`, data);
+    console.log("📦 전송할 주소:", `/api/car/cycle`);
     return response.data;
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -41,5 +41,13 @@ export default defineConfig({
     //     rewrite: (path) => path.replace(/^\/api/, ""),
     //   },
     // },
+       proxy: {
+      "/api": {
+        target: "http://tracky-hub-2020422079.ap-northeast-2.elb.amazonaws.com:8082", // Spring 서버 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+    
   },
 });
