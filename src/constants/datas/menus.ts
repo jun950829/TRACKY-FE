ef5ffe4f4f2f95be601e69f26cd646c42c4a9ce1
ji-won-2 <koojiwon990321@gmail.com>
2025-04-16 18:06:57 +0900

    feat: menus 수정

--- src/constants/datas/menus.ts
+++ src/constants/datas/menus.ts
@@ -15,29 +15,43 @@ export const headerMenus = [
     icon: Home
   },
   {
-    name: "차량 관리",
-    path: "/vehicles",
-    icon: Car
+    name: "차량",
+    path: "/cars",
+    icon: Car,
+    subMenus: [
+      {
+        name: "차량 관리",
+        path: "/cars",
+        icon: Car
+      },
+      {
+        name: "예약 관리",
+        path: "/rents",
+        icon: Car
+      }
+    ]
   },
   {
-    name: "운행 관리",
-    path: "/trips",
-    icon: FileText
+    name: "운행 기록",
+    path: "/history",
+    icon: FileText,
+    subMenus: [
+      {
+        name: "운행 일지",
+        path: "/history",
+        icon: FileText
+      },
+      {
+        name: "실시간",
+        path: "/history",
+        icon: FileText
+      }
+    ]
   },
   {
     name: "통계",
     path: "/statistics",
     icon: BarChart2
-  },
-  {
-    name: "회원 관리",
-    path: "/members",
-    icon: Users
-  },
-  {
-    name: "설정",
-    path: "/settings",
-    icon: Settings
   },
   {
     name: "관리자",
