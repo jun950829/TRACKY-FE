ea8cac1278cece92376e101a4a2bcbd774e1a7b2
Cheon SeungJun <55969783+jun950829@users.noreply.github.com>
2025-05-14 22:07:31 +0900

    feat: fix

--- src/pages/history/detail/HistoryMap.tsx
+++ src/pages/history/detail/HistoryMap.tsx
@@ -9,7 +9,6 @@ import {
   createPathSegments,
   PathSegment,
 } from '@/libs/utils/historyUtils';
-import { GpsData } from '@/constants/types/historyTypes';
 
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
